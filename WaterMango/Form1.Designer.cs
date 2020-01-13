54c35f18072619b907da889de3c6bcfa081a1d58
sjmathew <shawnmathew33@gmail.com>
2020-01-12 21:46:40 -0500

    Add files via upload

---
+++ WaterMango/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace WaterMango
+{
+    partial class Story2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Water Plant 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(16, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "-";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Water Plant 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(16, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "-";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Water Plant 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(16, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "-";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Water Plant 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(16, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "-";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 238);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Water Plant 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 1000;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(16, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "-";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 315);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(91, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Water All Plants";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(107, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(107, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(107, 128);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(10, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(107, 184);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(10, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "-";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(107, 238);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(10, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "-";
+            // 
+            // Story2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 399);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Story2";
+            this.Text = "WaterMango";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ WaterMango/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WaterMango
+{
+    public partial class Story2 : Form
+    {
+        public Story2()
+        {
+            InitializeComponent();
+        }
+
+        int time1 = 0;
+        int time2 = 0;
+        int time3 = 0;
+        int time4 = 0;
+        int time5 = 0;
+
+        string status1 = "";
+        string status2 = "";
+        string status3 = "";
+        string status4 = "";
+        string status5 = "";
+
+        bool check1 = false;
+        bool check2 = false;
+        bool check3 = false;
+        bool check4 = false;
+        bool check5 = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            check1 = story4(time1);
+
+            if(check1 == true)
+            {
+                timer1.Start();
+                time1 = 0;
+                label6.Text = "";
+            }
+
+            else
+            {
+                label6.Text = "Please wait until 30 seconds to water again";
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time1++;
+            status1 = story2(time1, status1);
+            label1.Text = status1.ToString();
+
+            if (story5(time1) == true)
+            {
+                label6.Text = "";
+                label1.Text = "It has been 60 seconds since last watering sessions";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            check2 = story4(time2);
+
+            if (check2 == true)
+            {
+                timer2.Start();
+                time2 = 0;
+                label7.Text = "";
+            }
+
+            else
+            {
+                label7.Text = "Please wait until 30 seconds to water again";
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            time2++;
+            status2 = story2(time2, status2);
+            label2.Text = status2.ToString();
+
+            if (story5(time2) == true)
+            {
+                label7.Text = "";
+                label2.Text = "It has been 60 seconds since last watering sessions";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            check3 = story4(time3);
+
+            if (check3 == true)
+            {
+                timer3.Start();
+                time3 = 0;
+                label8.Text = "";
+            }
+
+            else
+            {
+                label8.Text = "Please wait until 30 seconds to water again";
+            }
+
+            //timer3.Start();
+            //time3 = 0;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            time3++;
+            status3 = story2(time3, status3);
+            label3.Text = status3.ToString();
+
+            if (story5(time3) == true)
+            {
+                label8.Text = "";
+                label3.Text = "It has been 60 seconds since last watering sessions";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            check4 = story4(time4);
+
+            if (check4 == true)
+            {
+                timer4.Start();
+                time4 = 0;
+                label9.Text = "";
+            }
+
+            else
+            {
+                label9.Text = "Please wait until 30 seconds to water again";
+            }
+
+            //timer4.Start();
+            //time4 = 0;
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            time4++;
+            status4 = story2(time4, status4);
+            label4.Text = status4.ToString();
+
+            if (story5(time4) == true)
+            {
+                label9.Text = "";
+                label4.Text = "It has been 60 seconds since last watering sessions";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            check5 = story4(time5);
+
+            if (check5 == true)
+            {
+                timer5.Start();
+                time5 = 0;
+                label10.Text = "";
+            }
+
+            else
+            {
+                label10.Text = "Please wait until 30 seconds to water again";
+            }
+
+            //timer5.Start();
+            //time5 = 0;
+
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            time5++;
+            status5 = story2(time5, status5);
+            label5.Text = status5.ToString();
+
+            if(story5(time5) == true)
+            {
+                label10.Text = "";
+                label5.Text = "It has been 60 seconds since last watering sessions";
+            }
+        }
+
+        private string story2(int time, string status)
+        {
+            if (time >= 10)
+            {
+                status = "Watered " + time.ToString() + " seconds ago.";
+                return status;
+            }
+
+            else
+            {
+                status = "Watering... ( " + time.ToString() + " )";
+                return status;
+            }
+        }
+
+        private bool story4(int time)
+        {
+            if((time >= 30) || (time == 0))
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private bool story5(int time)
+        {
+            if(time >= 60)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            /*timer1.Start();
+            time1 = 0;
+
+            timer2.Start();
+            time2 = 0;
+
+            timer3.Start();
+            time3 = 0;
+
+            timer4.Start();
+            time4 = 0;*/
+
+            //timer5.Start();
+            //time5 = 0;
+
+            check1 = story4(time1);
+
+            if (check1 == true)
+            {
+                timer1.Start();
+                time1 = 0;
+                label6.Text = "";
+            }
+
+            else
+            {
+                label6.Text = "Please wait until 30 seconds to water again";
+            }
+
+            check2 = story4(time2);
+
+            if (check2 == true)
+            {
+                timer2.Start();
+                time2 = 0;
+                label7.Text = "";
+            }
+
+            else
+            {
+                label7.Text = "Please wait until 30 seconds to water again";
+            }
+
+            check3 = story4(time3);
+
+            if (check3 == true)
+            {
+                timer3.Start();
+                time3 = 0;
+                label8.Text = "";
+            }
+
+            else
+            {
+                label8.Text = "Please wait until 30 seconds to water again";
+            }
+
+            check4 = story4(time4);
+
+            if (check4 == true)
+            {
+                timer4.Start();
+                time4 = 0;
+                label9.Text = "";
+            }
+
+            else
+            {
+                label9.Text = "Please wait until 30 seconds to water again";
+            }
+
+            check5 = story4(time5);
+
+            if (check5 == true)
+            {
+                timer5.Start();
+                time5 = 0;
+                label10.Text = "";
+            }
+
+            else
+            {
+                label10.Text = "Please wait until 30 seconds to water again";
+            }
+
+        }
+    }
+}
